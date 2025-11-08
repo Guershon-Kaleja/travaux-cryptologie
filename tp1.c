518fe651a34b3bdf05b8001212463893f9734d05
Guershon-Kaleja <kalejashon@gmail.com>
2025-11-08 09:57:55 +0100

    tp1

--- tp1.c
+++ tp1.c
@@ -11,35 +11,30 @@ int main() {
     printf("Entrez le message à chiffrer : ");
     fgets(message, sizeof(message), stdin);
 
-    // Enlever le saut de ligne si présent
     int len = strlen(message);
     if (message[len - 1] == '\n')
         message[len - 1] = '\0';
 
-    // Remplacer les espaces par @
     for (i = 0; message[i] != '\0'; i++) {
         if (message[i] == ' ')
             message[i] = '@';
     }
 
-    // Déterminer automatiquement le nombre de colonnes
-    colonnes = (int) sqrt(strlen(message)); // racine carrée de la longueur
-    if (colonnes == 0) colonnes = 1;       // éviter zéro colonne
+    colonnes = (int) sqrt(strlen(message));
+    if (colonnes == 0) colonnes = 1;       
     lignes = strlen(message) / colonnes;
     if (strlen(message) % colonnes != 0)
         lignes++;
 
-    // Remplissage de la matrice ligne par ligne
     for (i = 0; i < lignes; i++) {
         for (j = 0; j < colonnes; j++) {
             if (k < strlen(message))
                 mat[i][j] = message[k++];
             else
-                mat[i][j] = '#'; // caractère de remplissage
+                mat[i][j] = '#';
         }
     }
 
-    // Lecture colonne par colonne pour chiffrer
     printf("\nTexte chiffré : ");
     for (j = 0; j < colonnes; j++) {
         for (i = 0; i < lignes; i++) {
